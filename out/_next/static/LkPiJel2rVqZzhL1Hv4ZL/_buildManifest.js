6d2fa1f4beaeddd00f62ac3a67ef2ade36dc4fe1
kombos <kamlesh@assetmantle.one>
2022-07-18 22:06:06 +0530

    Deploy gh-pages

---
+++ out/_next/static/LkPiJel2rVqZzhL1Hv4ZL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ebaf4c27-9f255e68013b3928.js","static/chunks/809-b5ea8afae02a01a8.js","static/chunks/pages/index-7662b05dae65157e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
