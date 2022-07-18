56eb8727b929b03de6ee5b31689e39f1d786b23f
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-07-18 20:06:19 +0600

    conpleted the header

--- components/AppBar.js
+++ components/AppBar.js
@@ -1,8 +1,20 @@
-import * as React from 'react';
-import MuiAppBar from '@mui/material/AppBar';
+import * as React from "react";
+import MuiAppBar from "@mui/material/AppBar";
+import { css } from "@emotion/react";
 
 function AppBar(props) {
-  return <MuiAppBar elevation={0} position="fixed" {...props} />;
+  return (
+    <MuiAppBar
+      elevation={0}
+      position="fixed"
+      css={css`
+        backdrop-filter: blur(2px);
+        border-radius: 5px;
+      `}
+      sx={{ backgroundColor: "rgba(4, 4, 4, 0.8)" }}
+      {...props}
+    />
+  );
 }
 
 export default AppBar;
