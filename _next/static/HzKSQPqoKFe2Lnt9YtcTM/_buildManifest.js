5e42183002b1d9adaae1cf40ee2450f9c048f6a4
kombos <kamlesh@assetmantle.one>
2022-07-20 10:09:53 +0530

    added updated carousel

--- _next/static/HzKSQPqoKFe2Lnt9YtcTM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/ebaf4c27-9f255e68013b3928.js","static/chunks/809-b5ea8afae02a01a8.js","static/chunks/pages/index-b4d63a7b55be60d6.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
