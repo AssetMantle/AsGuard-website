374686dcea2fd36e09d248a21cb27092381aea01
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-21 21:06:14 +0600

    💅gradient updated

--- views/HeroSection.js
+++ views/HeroSection.js
@@ -12,7 +12,13 @@ export default function HeroSection({ heroSectionConfigData }) {
       <div
         className="bg-image text-center d-flex flex-column justify-content-center align-items-center w-100 py-6 py-md-5"
         style={{
-          backgroundImage: `linear-gradient(0deg, rgba(14, 14, 14, 0) 41.37%, #0E0E0E 48.46%, #0E0E0E 61.44%, rgba(14, 14, 14, 0) 70.43%), url(${heroSectionConfigData.image})`,
+          backgroundImage: `linear-gradient(
+            0deg,
+            rgba(14, 14, 14, 0) 41.37%,
+            rgba(14, 14, 14, 0.8) 48.46%,
+            rgba(14, 14, 14, 0.8) 61.44%,
+            rgba(14, 14, 14, 0) 70.43%
+          ), url(${heroSectionConfigData.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center bottom",
           backgroundRepeat: "no-repeat",
